62801a549b32434e4778200442251eaa16e663da
Bryan Gordon <bryanbge92@gmail.com>
2025-02-17 18:56:04 -0500

    Update app structure

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup>
   import HeaderApp from './components/HeaderApp.vue';
   import FooterApp from './components/FooterApp.vue';
+  import Services from './components/Services.vue';
+  import Contactos from './components/Contactos.vue';
+  import Ubications from './components/Ubications.vue';
 </script>
 
 <template>
   <HeaderApp/>
+  <Services />
+  <Contactos />
+  <Ubications />
   <FooterApp />
-  
 </template>
